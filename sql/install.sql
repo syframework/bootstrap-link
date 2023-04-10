84203089b44477e9baabf73748584a9bc5882fee
Syone <syone7@gmail.com>
2023-04-10 20:28:16 +0200

    minor code format

--- sql/install.sql
+++ sql/install.sql
@@ -12,4 +12,5 @@ CREATE TABLE IF NOT EXISTS `t_link` (
   UNIQUE KEY `icon` (`icon`,`tag`) USING BTREE,
   KEY `tag` (`tag`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 SET FOREIGN_KEY_CHECKS=1;
